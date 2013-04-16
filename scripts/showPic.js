dd0fdbacb6683ce1214f1ad3016d96a2d670b0f5
Tylerdurdn <tylerdurdn2010@gmail.com>
2013-04-16 19:34:31 +0800

    final

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,26 +1,42 @@
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+	window.onload = func;
+}
+ else {
+	window.onload = function() {
+	oldonload();
+	func();
+			}
+	}
+}
+
+
 function showPic(whichpic) {
-/*var source = whichpic.getAttribute("href");
-var placeholder = document.getElementById("placeholder");
-placeholder.setAttribute("src",source);*/
-if (!document.getElementById("placeholder")) return false;
+if (!document.getElementById("placeholder")) return true;
 document.getElementById("placeholder").setAttribute("src",whichpic.getAttribute("href"));
 if (!document.getElementById("description")) return false;
-var text = whichpic.getAttribute("title");
+var text = whichpic.getAttribute("title")?whichpic.getAttribute("title") : "";
 var description = document.getElementById("description");
+if (description.firstChild.nodeType == 3) {
 description.firstChild.nodeValue = text;
+	}
+return false;
 }
 /*This alert statement will give you the value you¡¯re looking for:
 alert(description.childNodes[0].nodeValue);
 This will return a value of ¡°Choose an image.¡± This means that you¡¯re accessing the
 childNodes array and getting the value of the first element (index number zero).
 */
+addLoadEvent(prepareGallery);
 function prepareGallery() {
 	if (!document.getElementsByTagName) return false;
 	if (!document.getElementById) return false;
 	if (!document.getElementById("imagegallery")) return false;
 	var gallery = document.getElementById("imagegallery");
 	var links = gallery.getElementsByTagName("a");
-	for ( var i=0; i < links.length; i++) {
+	for (var  i=0; i < links.length; i++) {
+	alert(links[i]);
 	links[i].onclick = function() {
 	showPic(this);
 	return false;
@@ -47,18 +63,6 @@ window.open(winURL,"popup","width=320,height=480");
 }
 //window.onload = countBodyChildren;
 
-function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function') {
-	window.onload = func;
-}
- else {
-	window.onload = function() {
-	oldonload();
-	func();
-			}
-	}
-}
 
 
 
